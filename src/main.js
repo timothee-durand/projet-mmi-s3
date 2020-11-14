5041f6c92431437049b1621f98518738eee719a1
Khallan <a.pinot986@laposte.net>
2020-11-14 15:06:48 +0100

    Correction Bootstrap + setup fontawesome, Axios, Accueil
    Bootstrap-vue fonctionnel
    Import de Font Awesome pour les icons
    Import de axios pour les requêtes

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// utiliser bootstrap vue
+Vue.use(BootstrapVue)
+
+// en dev
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
