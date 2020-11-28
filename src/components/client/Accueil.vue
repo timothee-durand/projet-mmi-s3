4703e8d1404a2b9eb361921ac1179aa7613cabfc
Khallan <a.pinot986@laposte.net>
2020-11-28 17:53:30 +0100

    Header categories / Sidebar client
    Ajout du header pour selectionner les categories
    Ajout de la sidebar

---
+++ src/components/client/Accueil.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-alert show>Default Alert</b-alert>
+
+    <b-alert variant="success" show>Success Alert</b-alert>
+
+    <b-alert v-model="showDismissibleAlert" variant="danger" dismissible>
+      Dismissible Alert!
+    </b-alert>
+
+    <b-button @click="showDismissibleAlert=true" class="m-1" variant="info">
+      Show dismissible alert ({{ showDismissibleAlert ? 'visible' : 'hidden' }})
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Accueil',
+  data () {
+    return {
+      showDismissibleAlert: false
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
