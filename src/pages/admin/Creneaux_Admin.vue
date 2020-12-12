3b6a484002c2dab753b3ddd3e23ffefb0935aae1
timothee_durand <timothee.durand70@gmail.com>
2020-12-12 17:49:38 +0100

    add creneaux part to creneaux page

--- src/pages/admin/Creneaux_Admin.vue
+++ src/pages/admin/Creneaux_Admin.vue
@@ -17,7 +17,17 @@
         </b-col>
 
         <b-col cols="6">
+          <b-card>
+            <svg xmlns="http://www.w3.org/2000/svg" width="121.013" height="138.301" viewBox="0 0 121.013 138.301" class="d-block mx-auto">
+              <path id="Icon_awesome-calendar-alt" data-name="Icon awesome-calendar-alt" d="M0,125.335A12.969,12.969,0,0,0,12.966,138.3h95.082a12.969,12.969,0,0,0,12.966-12.966V51.863H0ZM86.438,72.392a3.251,3.251,0,0,1,3.241-3.241h10.8a3.251,3.251,0,0,1,3.241,3.241V83.2a3.251,3.251,0,0,1-3.241,3.241h-10.8A3.251,3.251,0,0,1,86.438,83.2Zm0,34.575a3.251,3.251,0,0,1,3.241-3.241h10.8a3.251,3.251,0,0,1,3.241,3.241v10.8a3.251,3.251,0,0,1-3.241,3.241h-10.8a3.251,3.251,0,0,1-3.241-3.241ZM51.863,72.392A3.251,3.251,0,0,1,55.1,69.15h10.8a3.251,3.251,0,0,1,3.241,3.241V83.2a3.251,3.251,0,0,1-3.241,3.241H55.1A3.251,3.251,0,0,1,51.863,83.2Zm0,34.575a3.251,3.251,0,0,1,3.241-3.241h10.8a3.251,3.251,0,0,1,3.241,3.241v10.8a3.251,3.251,0,0,1-3.241,3.241H55.1a3.251,3.251,0,0,1-3.241-3.241ZM17.288,72.392a3.251,3.251,0,0,1,3.241-3.241h10.8a3.251,3.251,0,0,1,3.241,3.241V83.2a3.251,3.251,0,0,1-3.241,3.241h-10.8A3.251,3.251,0,0,1,17.288,83.2Zm0,34.575a3.251,3.251,0,0,1,3.241-3.241h10.8a3.251,3.251,0,0,1,3.241,3.241v10.8a3.251,3.251,0,0,1-3.241,3.241h-10.8a3.251,3.251,0,0,1-3.241-3.241Zm90.76-89.679H95.082V4.322A4.335,4.335,0,0,0,90.76,0H82.116a4.335,4.335,0,0,0-4.322,4.322V17.288H43.219V4.322A4.335,4.335,0,0,0,38.9,0H30.253a4.335,4.335,0,0,0-4.322,4.322V17.288H12.966A12.969,12.969,0,0,0,0,30.253V43.219H121.013V30.253A12.969,12.969,0,0,0,108.047,17.288Z" fill="#81cb51"/>
+            </svg>
+            <h2 class="text-center">Créneaux</h2>
+            <p>Prochaines indisponibilités :</p>
 
+            <row-result id="0" ref="Mardi 21 sept. - 14h00" nom="Lundi 20 sept. - 14h00"  :buttons="buttonsCrnx" disable-dispo></row-result>
+
+            <router-link to="" class="btn rounded-pill bg-success text-light p-2 mx-auto d-block mt-4"  style="max-width: 15rem;">Configurer mes créneaux</router-link>
+          </b-card>
         </b-col>
       </b-row>
     </b-container>
@@ -31,19 +41,25 @@ import RowResult from '@/components/RowResult.vue'
 export default {
 name: "Creneaux_Admin",
   components: {RowResult},
-  computed: {
-    buttonsRdv () {
-      return [
+  data () {
+    return {
+      buttonsCrnx: [
         {
           icon:"check2",
           variant:"success",
           eventName:"validateRDV"
         },
+      ]
+    }
+  },
+  computed: {
+    buttonsRdv () {
+      return [
         {
-          icon:"trash-fill",
+          icon:"pencil-fill",
           variant:"success",
-          eventName:"deleteRDV"
-        },
+          eventName:"editIndispo"
+        }
       ]
     }
   },
