ec00659b8ddf81a270f20aebc715b7b531999df5
Timothée DURAND <timothee.durand70@gmail.com>
2021-01-05 14:54:59 +0100

    add col to orgganize content

--- src/components/RowResult.vue
+++ src/components/RowResult.vue
@@ -1,19 +1,28 @@
 <template>
-  <b-card>
-    <b-row  align-v="center" align-h="around">
-      <div >
-        <img v-if="!disableImg"  :src="img" class="rounded-pill" width="20px"  />
-      </div>
-         <p>{{nom}}</p>
-        <p v-if="!disableRef">{{reference}}</p>
-        <p :class="'text-'+dispo.variant" v-if="!disableDispo">{{dispo.text}}</p>
+  <b-card ref="rwo-result">
+    <b-row align-v="center" align-h="around">
+      <b-col cols="auto" md="1">
+        <img v-if="!disableImg" :src="img" class="rounded-pill" width="20px"/>
+      </b-col>
 
-        <div class="buttons">
-          <b-button v-for="button in buttons" @click="$emit(button.eventName, {nom, id})" :key="button.icon" variant="light" size="sm" class="mr-2">
-            <b-icon :icon="button.icon" :variant="button.variant"></b-icon>
-          </b-button>
-        </div>
+      <b-col cols="auto" md="6">
+        <p>{{ nom }}</p>
+        <p v-if="!disableRef">{{ reference }}</p>
+      </b-col>
 
+
+      <b-col cols="auto" md="3">
+        <p :class="'text-'+dispo.variant" v-if="!disableDispo">{{ dispo.text }}</p>
+      </b-col>
+
+      <b-col cols="auto" md="2">
+
+        <b-button v-for="button in buttons" @click="$emit(button.eventName, {nom, id})" :key="button.icon"
+                  variant="light" size="sm" class="mr-2">
+          <b-icon :icon="button.icon" :variant="button.variant"></b-icon>
+        </b-button>
+
+      </b-col>
 
 
     </b-row>
@@ -24,67 +33,93 @@
 export default {
   name: 'RowResult',
   props: {
-    id:{
-      type: [String,Number] ,
-      required:true
+    id: {
+      type: [String, Number],
+      required: true
     },
     img: {
       type: String,
-      default: "http://via.placeholder.com/100x100"
+      default: './assets/img/person.png'
     },
-    nom:{
+    nom: {
       type: String,
-      required:true,
-      default:"Kit 01"
+      required: true,
+      default: 'Kit 01'
     },
-    reference:{
+    reference: {
       type: String,
-      default:"L1-MAL-PHOT-01"
+      default: 'L1-MAL-PHOT-01'
     },
-    dispo:{
+    dispo: {
       type: Object,
       default: function () {
         return {
-          text:"Disponible",
-          variant:"success"
+          text: 'Disponible',
+          variant: 'success'
         }
       }
     },
-    disableDispo:{
-      type:Boolean,
-      default:false
+    disableDispo: {
+      type: Boolean,
+      default: false
     },
-    disableImg:{
-      type:Boolean,
-      default:false
+    disableImg: {
+      type: Boolean,
+      default: false
     },
-    disableRef:{
-      type:Boolean,
-      default:false
+    disableRef: {
+      type: Boolean,
+      default: false
     },
-    buttons:{
-      type:Array,
-      default:function () {
+    buttons: {
+      type: Array,
+      default: function () {
         return [
           {
-            icon:"lock",
-            variant:"success",
-            eventName:"setUnavailable"
+            icon: 'lock',
+            variant: 'success',
+            eventName: 'setUnavailable'
           }
         ]
       }
+    },
+    wDisCols:{
+      type:Number,
+      default:400
     }
+  },
+  computed: {
+    variableCols(){
+      if (this.$refs["rwo-result"].clientWidth < this.wDisCols) {
+        return {
+          img:"auto",
+          txt:"auto",
+          dispo:"auto",
+          buttons:"auto",
+        }
+      } else {
+        return {
+          img:1,
+          txt:6,
+          dispo:3,
+          buttons:2
+        }
+      }
+
+
+    }
+  ,
   },
 }
 </script>
 
 <style scoped>
-  p{
-    margin: 0;
-  }
+p {
+  margin: 0;
+}
 
-  .card-body{
-    padding-top: 0.4rem !important;
-    padding-bottom: 0.4rem !important;
-  }
+.card-body {
+  padding-top: 0.4rem !important;
+  padding-bottom: 0.4rem !important;
+}
 </style>
